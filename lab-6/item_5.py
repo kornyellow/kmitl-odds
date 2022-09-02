972e5ff3ff937db89ddb924594f63b8c3d61661a
kornyellow <kornkubzaza@gmail.com>
2022-09-02 15:55:19 +0700

    Change for, while to recursion

--- lab-6/item_5.py
+++ lab-6/item_5.py
@@ -3,15 +3,18 @@ def sign(x):
         return 0
     return 1 if x > 0 else -1
 
+def append_repeat(times, item, i=0):
+    if i == times:
+        return []
+    return [item] + append_repeat(times, item, i+1)
+
 def staircase(m, i=0):
     if i == 0:
         i = 1 if m > 0 else abs(m)
 
     ret = []
-    for _ in range(abs(m)-i):
-        ret.append("_")
-    for _ in range(i):
-        ret.append("#")
+    ret += append_repeat(abs(m)-i, "_")
+    ret += append_repeat(i, "#")
 
     if i == (m if m > 0 else 1):
         return "".join(ret)
